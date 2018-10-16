849ea8d6b49cedf7b032c5f467650a4cd9e967c7
kev <kelvinchege@gmail.com>
2018-10-16 20:21:21 +0300

    new design

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SakaPro' });
 });
 
+router.get('/terms-conditions', function(req, res, next){
+  res.render('site/terms',{title: 'SakaPro: Terms and Conditions'});
+});
+
 module.exports = router;
