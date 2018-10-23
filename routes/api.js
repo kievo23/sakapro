027cc3f57892e61f3fc31e887094aada66617d8f
kev <kelvinchege@gmail.com>
2018-10-23 22:53:34 +0300

    commit

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
+
+var request = require("request");
+
 var User = require(__dirname + '/../models/User');
 var Prof = require(__dirname + '/../models/Pro');
 var Category = require(__dirname + '/../models/Category');
 
 router.post('/user/create',function(req, res){
   var code = Math.floor((Math.random() * 9999) + 1000);
+  var phone = req.body.phone.replace(/\s+/g, '');
+  phone = "254"+phone.substr(phone.length - 9);
   User.create({
     names : req.body.names,
     email: req.body.email,
-    phone: req.body.phone,
+    phone: phone,
     facebookid: req.body.facebookid,
     googleid: req.body.googleid,
     otp: code
@@ -50,7 +55,25 @@ router.post('/user/generateotp',function(req, res){
         if(err){
           res.json({code:101, msg: "something went wrong"});
         }else{
-          res.json({code:100, msg: "OTP generated successfully"});
+          var options = { method: 'GET',
+            url: 'http://infopi.io/text/index.php',
+            qs:
+             { app: 'ws',
+               u: 'Goodlife',
+               h: 'a425d383d0af9d75a9ab1db94747e441',
+               op: 'pv',
+               to: d.phone,
+               msg: 'OTP code is: '+ code },
+            headers:
+             { 'postman-token': '4ca47976-a3bc-69d6-0cae-e80049f926e9',
+               'cache-control': 'no-cache' } };
+
+          request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            res.json({code:100, msg: "OTP generated successfully"});
+            //console.log(body);
+          });
+
         }
       })
     }else{
@@ -61,11 +84,13 @@ router.post('/user/generateotp',function(req, res){
 
 router.post('/prof/create',function(req, res){
   var code = Math.floor((Math.random() * 9999) + 1000);
+  var phone = req.body.phone.replace(/\s+/g, '');
+  phone = "254"+phone.substr(phone.length - 9);
   Prof.create({
     nickname: req.body.nickname,
     names : req.body.names,
     email: req.body.email,
-    phone: req.body.phone,
+    phone: phone,
     dob: req.body.dob,
     email: req.body.email,
     pin: req.body.pin,
@@ -134,6 +159,8 @@ router.post('/user/verifyg',function(req, res){
     }
   });
 });
+
+
 
 router.get('/category', function(req, res){
   Category.find({}).then(function(d){
