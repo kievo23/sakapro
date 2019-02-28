b4b8169c1e79fba31cb4f3e4ef3e233055ff2119
kev <kelvinchege@gmail.com>
2019-02-28 14:57:57 +0300

    edit prof

--- routes/prof.js
+++ routes/prof.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Prof = require(__dirname + '/../models/Pro');
+var Category = require(__dirname + '/../models/Category');
 var role = require(__dirname + '/../config/Role');
 
 /* GET profs listing. */
@@ -35,8 +36,10 @@ router.get('/view/:id', function(req, res){
 });
 
 router.get('/update/:id', function(req, res){
-  var prof = Prof.findById(req.params.id).then(function(prof){
-    res.render('prof/update',{prof: prof});
+  var cats = Category.find({}).populate('group');
+  var prof = Prof.findById( req.params.id ).populate('jobtype');
+  Promise.all([ cats, prof ]).then(values => {
+    res.render('profs/update',{ prof: values[1], category: values[0]});
   });
 });
 
@@ -48,13 +51,18 @@ router.get('/delete/:id', function(req, res){
   });
 });
 
-router.post('/edit/:id',function(req, res){
-  var prof = Prof.findById(req.params.id).then(function(category){
-    prof.name = req.body.name;
-    prof.save(function(err){
-      if(err) throw console.log(err);
-      res.redirect('/category');
-    });
+router.post('/edit',function(req, res){
+    Prof.findById(req.body.id).then(function(prof){
+      console.log(req.body);
+      prof.names = req.body.name;
+      prof.jobtype = req.body.category;
+      prof.idno = req.body.idno;
+      prof.email = req.body.email;
+      prof.phone = req.body.phone;
+      prof.save(function(err){
+        if(err) throw console.log(err);
+        res.redirect('/prof/view/'+req.body.id);
+      });
   });
 });
 
